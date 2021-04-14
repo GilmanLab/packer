b3f70bd11251cdfcd41f60903085be3c8ee7c806
Joshua Gilman <joshuagilman@gmail.com>
2021-04-14 11:38:33 -0700

    Moves passwords to environment variables instead of CLI arguments for Ubuntu builds

--- ubuntu/build.ps1
+++ ubuntu/build.ps1
@@ -1,8 +1,5 @@
 param(
     [string] $Type,
-    [string] $Username,
-    [string] $VspherePassword,
-    [string] $AdminPassword,
     [string] $Image
 )
 
@@ -10,18 +7,18 @@ switch ($Type) {
     iso {
         & packer build `
             -force `
-            -var "vsphere_username=$Username" `
-            -var "vsphere_password=$VspherePassword" `
-            -var "admin_password=$AdminPassword" `
+            -var "vsphere_username=$env:VSPHERE_USER" `
+            -var "vsphere_password=$env:VSPHERE_PASS" `
+            -var "admin_password=$env:ADMIN_PASS" `
             -var-file "$Image/config.pkrvars.hcl" `
             iso.pkr.hcl
     }
     clone {
         & packer build `
             -force `
-            -var "vsphere_username=$Username" `
-            -var "vsphere_password=$VspherePassword" `
-            -var "admin_password=$AdminPassword" `
+            -var "vsphere_username=$env:VSPHERE_USER" `
+            -var "vsphere_password=$env:VSPHERE_PASS" `
+            -var "admin_password=$env:ADMIN_PASS" `
             -var-file "$Image/config.pkrvars.hcl" `
             clone.pkr.hcl
     }
